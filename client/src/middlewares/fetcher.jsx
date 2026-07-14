25f9596e1179cfb9ffe10159b8be626b1893f929
ahmadjon09 <youtobe83a61a@gmail.com>
2026-07-14 13:43:56 +0500

    fixed url

--- client/src/middlewares/fetcher.jsx
+++ client/src/middlewares/fetcher.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import Cookies from 'js-cookie'
 
-const BASE_URL = `http://localhost:5000/api/v1`
+const BASE_URL = `https://ofm-r37o.onrender.com/api/v1`
 localStorage.setItem('base_url', BASE_URL)
 const token = Cookies.get('user_token')
 const instance = axios.create({
